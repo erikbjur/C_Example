3aae9c63d1b0ee5202a7aa8f5d345d7945ca354c
Erik Bjur <erik@bjurengineering.com>
2023-08-27 18:43:50 -0700

    Changed end number to 1000

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 
 int main() {
     printf("Hello world\n");
-    printf("Hit enter to count to 10" );
+    printf("Hit enter to count to 1000" );
     //Wait for the user to hit a key
     getchar();
 
     //Now count to 10
-    for(int i = 1; i < 10+1; i++) {
+    for(int i = 1; i < 1000+1; i++) {
         printf("%i\n", i);
     }
 
